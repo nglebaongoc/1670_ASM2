2d763a76263dd7150df861a7f21c513162d3c473
zinu <GiangNTGCH190083@fpt.edu.vn>
2022-12-17 13:21:40 +0700

    chang

---
+++ Asm2/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asm2.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public string? Category { get; set; }
+        [Required]
+        public int? Quatity { get; set; }
+        [Required]
+        public DateTime Year { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
